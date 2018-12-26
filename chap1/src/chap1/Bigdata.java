8d0678a92b2efa4180d648106c5597960944753d
student <student@M100324>
2018-12-26 13:49:35 +0900

    1.최초업로드

---
+++ chap1/src/chap1/Bigdata.java
@@ -0,0 +1,16 @@
+package chap1;
+
+public class Bigdata {
+
+	public static void main(String[] args) {
+		//자바 변수 선언 저장 
+		String title="r과 파이썬 과정";
+		
+		//변수 사용
+		System.out.println("저는 " + title + "의 \n임지혜입니다");
+		System.out.println(title+ "은 12월 26일부터 "
+				+"2월 28일까지 수업합니다.");
+
+	}
+
+}
