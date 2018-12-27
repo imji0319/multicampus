0aa46612e3578cfdf5debee3455d39af1cb9cc07
JIHYE LIM <tfj0531@gmail.com>
2018-12-27 16:22:55 +0900

    Day2

--- chap4/src/chap4/NumStar.java
+++ chap4/src/chap4/NumStar.java
@@ -4,6 +4,7 @@ public class NumStar {
 
 	public static void main(String[] args) {
 		int num=(int)(Math.random()*10)+1; //1-10 사이의 난수 발생
+		System.out.println(num+"의 별");
 		int i=1;
 		int j=1;
 		for (i=1; i<=num; i++) {
@@ -12,7 +13,6 @@ public class NumStar {
 			}
 			System.out.println("\n");
 		}
-		System.out.println(num+"의 별");
 	}
 
 }
