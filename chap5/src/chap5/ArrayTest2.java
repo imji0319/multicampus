93b246023c5d617b42d5d3b06e7b9b79e6ab9489
JIHYE LIM <tfj0531@gmail.com>
2018-12-28 13:44:30 +0900

    Day3

--- chap5/src/chap5/ArrayTest2.java
+++ chap5/src/chap5/ArrayTest2.java
@@ -9,6 +9,7 @@ public class ArrayTest2 {
 		
 		for (int i=0 ; i<scores.length ;i++) {
 			scores[i]=(int)(Math.random()*100)+1;
+			System.out.print(scores[i]+"\t");
 			if (max <scores[i]) {
 				max = scores[i];
 			}
@@ -16,12 +17,16 @@ public class ArrayTest2 {
 				min =scores[i];
 			}
 			
+			if (scores[0]<scores[i]) {
+				scores[0]=scores[i];
+			}
 			System.out.print(scores[i]+"\t");
 		}
 	
 		
 		System.out.println("최대값 : " + max);
 		System.out.println("최소값 : " + min);
+		System.out.println("최대값 : " + scores[0]);
 		
 		//System.out.println(scores);
 
