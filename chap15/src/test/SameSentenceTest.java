06196990fcc2981f63dd2e995146d9983eb3ba5e
JIHYE LIM <tfj0531@gmail.com>
2019-01-08 09:07:56 +0900

    if 문 수정

--- chap15/src/test/SameSentenceTest.java
+++ chap15/src/test/SameSentenceTest.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 class SameSentence{
 
 	String[] compare(ArrayList<String> list1, ArrayList<String> list2) {
+		
 		ArrayList<String> arr = new ArrayList<String>();
+		
 		if (list1.size() == list2.size()) {
 			for (int i =0; i < list1.size();i++) {
 				if (list1.get(i) == list2.get(i)) {
@@ -13,13 +15,24 @@ class SameSentence{
 				}
 			}
 		}
-		else {
-			for (int i =0; i < list2.size();i++) {
+		
+		//list1.size() > list2.size()
+		else if (list1.size() > list2.size()){ 
+			for (int i =0; i < list2.size(); i++) {
 				if (list1.get(i) == list2.get(i)) {
 					arr.add(list1.get(i));
 				}
 			}
 		}
+		
+		else {
+			for (int i =0; i < list2.size(); i++) {
+				if (list1.get(i) == list2.get(i)) {
+					arr.add(list1.get(i));
+				}
+			}
+		}
+
 		
 		String[] result2 = arr.toArray(new String[arr.size()] );
 		
@@ -51,6 +64,7 @@ public static void main(String[] args) {
 	list3.add("우리는 kitri에서 자바를 배우는 중입니다");
 	list3.add("오늘은 콜렉션 프레임워크를 배우죠! ");
 	
+	
 	SameSentence ss = new SameSentence();
 	String[] result = ss.compare(list1, list2);//list1, list2에서 같은 내용 출력
 	
@@ -60,7 +74,6 @@ public static void main(String[] args) {
 		
 		System.out.println(result[i]);
 		}
-		//}
 
 	String[] result2= ss.compare(list1, list3);//list3의 갯수만큼만 같은 내용 출력	
 	for (int i=0; i<result2.length;i++) {
