26f7934cd6b325109a13051619ebc2970fb9eaba
JIHYE LIM <tfj0531@gmail.com>
2019-01-12 14:06:55 +0900

    Day13_sql insert delete update

--- test2/test_3.sql
+++ test2/test_3.sql
@@ -7,13 +7,18 @@ create table member( id varchar2(10),
 					
 select tname from tab;
 
+
+
 alter table member add(sex varchar2(2));
 
 alter table member modify(email number(3));
 
 alter table member drop column sex;
 
+
 drop table member;
+
+
 
 create table member( id varchar2(10) constraint member_id_pk primary key,
 					password number(12) constraint member_pw_nn not null,
@@ -21,6 +26,7 @@ create table member( id varchar2(10) constraint member_id_pk primary key,
 					phone varchar2(20) constraint member_phone_ck check(phone like '010%') ,
 					email varchar2(25) constraint member_email_uk unique,
 					indate date);
+					
 
 insert into member(id, password, name, phone, email, indate)
 		values('oracle',11111,'홍길도','010-1452-4152','tbjh@multi.com',sysdate); 
@@ -28,8 +34,12 @@ insert into member(id, password, name, phone, email, indate)
 
 insert into member(id, password)
 		values('oracl2',11111)
+		
 insert into member(id, password, name, phone, email, indate)
 		values('oracle3',11111,NULL,NULL,NULL,NULL); 			
+		
+insert into member(id, password)
+		values('oracle4',11111)
 			
 insert into member(ID, PASSWORD)
 			select substr(first_name, 1,5), employee_id 
@@ -43,8 +53,37 @@ insert into member(ID, EMAIL,PASSWORD)
 			select EMPLOYEE_ID,EMAIL, manager_id 
 			from employees where first_name !='Steven';
 		
+insert into member
+		values('oracle4',11111,'홍길도','017-1452-4152','ora4@multi.com',sysdate); 
+
+				
+insert into member
+		values('oracle5',11111,'이자바','010-1452-4152','orda1@multi.com',sysdate); 
+
 		
-select * from member;
+
+			
+delete member where ID like '1%' or ID like'2%' or ID like'3%'
+
+
+
+
+update member set name = '이길동', phone = '010-9999-0987',indate=sysdate
+		where id = 'oracle4'
+
+update member set name = '이길동', indate=sysdate
+		where id = 'oracle3'		
+		
+
+select* from member where phone like '%0987'
+
+update member set indate = sysdate+1
+		where to_char(indate,'yyyy-mm-dd') = (select to_char(indate,'yyyy-mm-dd')
+											  from member
+											  where phone like '%0987')
+
+select * from member
+
 
 
 
