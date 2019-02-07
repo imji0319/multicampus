8cdadec1ae9791d49b9870b8a018627fe1061a0d
imji0319 <tfj0531@gmail.com>
2019-02-07 16:33:33 +0900

    교재

--- rProject/daumnews.R
+++ rProject/daumnews.R
@@ -5,11 +5,11 @@ url<- "http://media.daum.net/ranking/popular/"
 text <- read_html(url,  encoding="utf-8")
 
 #뉴스제목 
-nodes <- html_nodes(text, ".tit_thumb .link_txt")
+nodes <- html_nodes(text, ".list_news2 .tit_thumb> .link_txt")
 newstitle <- html_text(nodes,trim=TRUE)
 
 #신문사
-nodes <- html_nodes(text, ".info_news")
+nodes <- html_nodes(text, ".list_news2 .tit_thumb> .info_news")
 newspapername <- html_text(nodes,trim=TRUE)
 
 newstitle<-gsub("\n", "", newstitle) 
--- rProject/r_Day4.R
+++ rProject/r_Day4.R
@@ -114,3 +114,158 @@ doc <- xmlParse(url)
 top <- xmlRoot(doc); top
 df <- xmlToDataFrame(getNodeSet(doc, "//itemList"))
 df
+
+
+
+
+
+# 기본 시각화 API 학습
+
+국어<- c(4,7,6,8,5,5,9,10,4,10)  
+plot(국어)
+
+plot(국어, type="o", col="blue")       
+title(main="성적그래프", col.main="red", font.main=4) 
+
+국어 <- c(4,7,6,8,5,5,9,10,4,10)
+수학 <- c(7,4,7,3,8,10,4,10,5,7)
+
+plot(국어, type="o", col="blue")
+lines(수학, type="o", pch=16, lty=2, col="red")     
+title(main="성적그래프", col.main="red", font.main=4)
+
+국어 <- c(4,7,6,8,5,5,9,10,4,10)
+par(mar=c(1,1,1,1), mfrow=c(4,2))
+
+plot(국어, type="p", col="blue", main="type = p", xaxt="n", yaxt="n")
+plot(국어, type="l", col="blue", main="type = l", xaxt="n", yaxt="n")
+plot(국어, type="b", col="blue", main="type = b", xaxt="n", yaxt="n")
+plot(국어, type="c", col="blue", main="type = c", xaxt="n", yaxt="n")
+plot(국어, type="o", col="blue", main="type = o", xaxt="n", yaxt="n")
+plot(국어, type="h", col="blue", main="type = h", xaxt="n", yaxt="n")
+plot(국어, type="s", col="blue", main="type = s", xaxt="n", yaxt="n")
+plot(국어, type="S", col="blue", main="type = S", xaxt="n", yaxt="n")
+
+국어 <- c(4,7,6,8,5,5,9,10,4,10); 
+수학 <- c(7,4,7,3,8,10,4,10,5,7)
+par(mar=c(5,5,5,5), mfrow=c(1,1))
+plot(국어, type="o", col="blue", ylim=c(0,10), axes=FALSE, ann=FALSE) 
+
+# x, y 축 추가하기
+axis(1, at=1:10, lab=c("01","02","03","04", "05","06","07","08","09","10")) # x축 추가
+axis(2, at=c(0,2,4,6,8,10))  # y축 추가
+
+# 그래프 추가하고, 그래프에 박스 그리기
+lines(수학, type="o", pch=16, lty=2, col="red")    
+box()   # 박스 그리기
+
+# 그래프 제목, 축의 제목, 범례 나타내기
+title(main="성적그래프", col.main="red", font.main=4) 
+title(xlab="학번", col.lab=rgb(0,1,0)) title(ylab="점수", col.lab=rgb(1,0,0)) 
+legend(1, 10, c("국어","수학"), cex=0.8, col=c("blue","red"), pch=c(16,21), lty=c(1,2))  
+
+
+(성적 <- read.table("성적.txt", header=TRUE));
+
+plot(성적$학번, 성적$국어, main="성적그래프", xlab="학번", ylab="점수",  xlim=c(0, 11), ylim=c(0, 11)) 
+
+ymax <- max(성적[3:5]) #성적 데이터 중에서 최대값을 찾는다(y 축의 크기 제한)
+ymax
+pcols<- c("red","blue","green")
+png(filename="성적.png", height=400, width=700, bg="white") # 출력을 png파일로 설정
+plot(성적$국어, type="o", col=pcols[1], ylim=c(0, ymax), axes=FALSE, ann=FALSE)
+axis(1, at=1:10, lab=c("01","02","03","04","05","06","07","08","09","10"))
+axis(2, at=0:5, lab=c(0,2,4,6,8,10))
+box()
+lines(성적$수학, type="o", pch=16, lty=2, col=pcols[2])
+lines(성적$영어, type="o", pch=23, lty=3, col=pcols[3] )
+title(main="성적그래프", col.main="red", font.main=4)
+title(xlab="학번", col.lab=rgb(1,0,0))
+title(ylab="점수", col.lab=rgb(0,0,1))
+legend(1, ymax, names(성적)[-1], cex=0.8, col=pcols, pch=c(21,16,23), lty=c(1,2,3))
+dev.off()
+
+# 막대그래프 그리기
+
+barplot(국어)
+
+coldens <- seq(from=10, to=100, by=10)   # 막대그래프의 색밀도 설정을 위한 벡터
+xname <- 성적$학번                                         # X 축 값 설정위한  벡터
+barplot(성적$국어, main="성적그래프", xlab="학번", ylab="점수", border="red", col="green", density=coldens, names.arg=xname)
+
+# 학생의 각 과목에 대한 각각의 점수에 대한 그래프
+성적1<- 성적[3:5]                                           
+barplot(as.matrix(성적1), main="성적그래프", ylab="점수", beside=TRUE, col=rainbow(10))
+
+
+# 학생의 각 과목에 대한 합계 점수에 대한 그래프
+xname <- 성적$학번;    #  x축 레이블용 벡터
+par(xpd=T, mar=par()$mar+c(0,0,0,4));   # 우측에 범례가 들어갈 여백을 확보
+barplot(t(성적1), main="성적그래프", ylab="점수", col=rainbow(3), space=0.1, cex.axis=0.8, names.arg=xname, cex=0.8)
+legend(11,30, names(성적1), cex=0.8, fill=rainbow(3));
+
+# 학생의 각 과목에 대한 합계 점수에 대한 그래프(가로막대 그래프)
+xname <- 성적$학번;    #  x축 레이블용 벡터
+barplot(t(성적1), main="성적그래프", ylab="학번", col=rainbow(3), space=0.1, cex.axis=0.8, names.arg=xname, cex=0.8, horiz=T);
+legend(30,11, names(성적1), cex=0.8, fill=rainbow(3));
+
+# 히스토그램
+hist(성적$국어, main="성적분포-국어", xlab="점수", breaks=5, col = "lightblue", border = "pink")
+hist(성적$수학, main="성적분포-수학", xlab="점수", col = "lightblue", border = "pink")
+hist(성적$국어, main="성적분포-국어", xlab="점수", ylab="도수", col=rainbow(12), border = "pink")
+
+nums <- sample(1:100, 30)
+hist(nums)
+hist(nums, breaks=c(0,33,66,100))
+
+
+# 파이그래프
+(성적 <- read.table("성적.txt", header=TRUE));
+pie(성적$국어, labels=paste(성적$성명, "-", 성적$국어), col=rainbow(10))
+pie(성적$국어, labels=paste(성적$성명, "-", 성적$국어), col=rainbow(10), main="국어성적", edges=10)
+pie(성적$국어, labels=paste(성적$성명,"\n","(",성적$국어,")"), col=rainbow(10))
+
+
+
+
+# 박스플롯
+summary(성적$국어)
+boxplot(성적$국어, col="yellow",  ylim=c(0,10), xlab="국어", ylab="성적")
+
+
+성적2 <- 성적[,3:5]
+boxplot(성적2, col=rainbow(3), ylim=c(0,10), ylab="성적")
+
+data <- read.table("온도.txt", header=TRUE, sep=",")
+head(data, n=5); 
+boxplot(data)
+boxplot(data, las = 2)
+boxplot(data, las = 2, at = c(1,2,3,4, 6,7,8,9, 11,12,13,14))
+chtcols = rep(c("red","sienna","palevioletred1","royalblue2"), times=3)
+chtcols
+boxplot(data, las = 2, at = c(1,2,3,4, 6,7,8,9, 11,12,13,14), col=chtcols)
+grid(col="gray", lty=2, lwd=1)
+
+rainbow()
+heat.colors()
+terrain.colors()
+topo.colors()
+cm.colors()
+gray.colors()
+
+install.packages("ggplot2")
+library(ggplot2)
+mpg<-as.data.frame(ggplot2::mpg)
+str(mpg)
+?mpg
+
+#엑셀파일 불러오기
+
+install.packages("readxl")
+library(readxl)
+setwd("C:/Users/student/Documents/multicampus/rProject")
+df_exam <- read_excel("excel_exam.xlsx")
+df_exam
+
+df_exam_sheet <- read_excel("excel_exam_sheet.xlsx", sheet=3)
+df_exam_sheet
